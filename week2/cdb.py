8766a4b9e89883d2479d28b92632e1e9e6123e6f
JasperK04 <j.kleine.4@student.rug.nl>
2024-02-13 17:08:43 +0100

    cdb.py version 1.0

---
+++ week2/cdb.py
@@ -0,0 +1,27 @@
+import xml.etree.ElementTree as ET
+import sys
+
+def get_adjectives(filename: str = "cdb-sample.xml") -> set:
+    adjectives = set()
+    tree = ET.parse(filename)
+    cdbid = tree.getroot()
+    for cid in cdbid:
+        pos = cid.attrib["pos"]
+        if pos == "ADJ" and cid.attrib["form"] != "":
+            adjectives.add(cid.attrib["form"])
+
+    return adjectives
+
+
+def main():
+    if len(sys.argv) > 1:
+        adj = sorted(get_adjectives(sys.argv[1]))
+        for word in adj:
+            print(word)
+    else:
+        adj = get_adjectives()
+        print(adj)
+
+
+if __name__ == "__main__":
+    main()
